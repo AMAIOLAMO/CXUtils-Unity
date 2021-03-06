b094d87bd7098d566e599448ea67d3677bf016b2
CXRedix <1135058679@qq.com>
2021-03-06 17:19:38 +0800

    Testing sub behaviours

---
+++ Scripts/NonEditorScripts/SubMonoBehaviour/SubMonoBehaviour.cs
@@ -0,0 +1,14 @@
+namespace CXUtils.CodeUtils
+{
+    /// <summary>
+    /// This is a class that helps you implement more optimized and less monobehaviour reflection calls :D
+    /// </summary>
+    public abstract class SubMonoBehaviour
+    {
+        public virtual void Awake() { }
+        public virtual void Start() { }
+        public virtual void Update() { }
+        public virtual void FixedUpdate() { }
+    }
+}
+
