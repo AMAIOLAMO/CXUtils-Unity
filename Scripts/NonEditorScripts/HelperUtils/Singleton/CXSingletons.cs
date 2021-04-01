1f151329834fddc4c2cf10407e4ed23d15fca2e7
CXRedix <1135058679@qq.com>
2021-04-01 16:28:13 +0800

    Added singleton helpers

---
+++ Scripts/NonEditorScripts/HelperUtils/Singleton/CXSingletons.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+
+namespace CXUtils.DesignPatterns
+{
+    /// <summary>
+    /// A base singleton class that you can inherit to control from <br/>
+    /// This class doesn't Do the <see cref="MonoBehaviour.DontDestroyOnLoad"/> method
+    /// </summary>
+    public abstract class Singleton<T> : MonoBehaviour where T : Component
+    {
+        protected static T _instance;
+
+        /// <summary>
+        /// The instance of this class
+        /// </summary>
+        public static T Instance => GetInstance();
+
+        protected static bool _isAppQuitting = false;
+
+        /// <summary>
+        /// Get's an instance of this singleton class
+        /// </summary>
+        public static T GetInstance()
+        {
+            if (_isAppQuitting)
+            {
+                Debug.LogWarning("Application is quitting! cannot get instance! of type: " + typeof(T).Name);
+                return null;
+            }
+            
+            if (_instance != null) return _instance;
+
+            _instance = FindObjectOfType<T>();
+
+            //if can find it now, then return
+            if (_instance != null)
+                return _instance;
+            
+            //else just create it
+            var obj = new GameObject { name = typeof(T).Name };
+            
+            _instance = obj.AddComponent<T>();
+
+            return _instance;
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+                _instance = this as T;
+            
+            else if (_instance != this as T)
+                Destroy(gameObject);
+        }
+
+        private void OnApplicationQuit() => _isAppQuitting = true;
+    }
+
+    /// <summary>
+    /// A singeton that auto does the <see cref="MonoBehaviour.DontDestroyOnLoad"/> method for you
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class DontDestroySingleton<T> : Singleton<T> where T : Component
+    {
+        protected override void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (_instance != this as T)
+            {
+                Destroy(gameObject);
+            }
+            else
+                DontDestroyOnLoad(gameObject);
+        }
+    }
+}
