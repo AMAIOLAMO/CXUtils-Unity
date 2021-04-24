5f34f2f24aa1a7600efd067c2f5267d16de42fbf
CXRedix <1135058679@qq.com>
2021-04-24 15:15:03 +0800

    Fixed and updated ObjectFollower

--- Scripts/NonEditorScripts/HelperComponents/Objects/ObjectFollower.cs
+++ Scripts/NonEditorScripts/HelperComponents/Objects/ObjectFollower.cs
@@ -1,60 +1,76 @@
-﻿using UnityEngine;
-
+﻿using System;
+using CXUtils.CodeUtils;
+using UnityEngine;
+using UnityEngine.Serialization;
 namespace CXUtils.HelperComponents
 {
-    [AddComponentMenu("CXUtils/Objects/ObjectFollower")]
+    [AddComponentMenu( "CXUtils/Objects/ObjectFollower" )]
     public class ObjectFollower : MonoBehaviour
     {
         #region Enums
+
         /// <summary> Option flags for the object to follow the position </summary>
         public enum ObjectFollowPositionOptions
-        { None, All, HasOffsetOnly, HasLerpOnly }
+        {
+            None, All, HasOffsetOnly, HasLerpOnly
+        }
 
         /// <summary> Option flags for the object to follow the rotation </summary>
         public enum ObjectFollowRotationOptions
-        { None, HasLerp, NoLerp }
+        {
+            None, HasLerp, NoLerp
+        }
 
         /// <summary> Option flags for the update mode for the follower </summary>
         public enum ObjectUpdateOptions
-        { Update, FixedUpdate, LateUpdate }
+        {
+            Update, FixedUpdate, LateUpdate
+        }
+
+        public enum ObjectDeltaTimeOptions
+        {
+            None, Normal, Fixed
+        }
 
         #endregion
 
         #region Fields
-        [Header("Configuration")]
+
+        [Header( "Configuration" )]
         public Transform transformTo;
 
-        public ObjectFollowPositionOptions objectFollowPositionOptions = ObjectFollowPositionOptions.All;
-        public ObjectFollowRotationOptions objectFollowRotationOptions = ObjectFollowRotationOptions.None;
-        public ObjectUpdateOptions objectUpdateOptions = ObjectUpdateOptions.LateUpdate;
+        public ObjectFollowPositionOptions positionOptions = ObjectFollowPositionOptions.All;
+        public ObjectFollowRotationOptions rotationOptions = ObjectFollowRotationOptions.None;
+        public ObjectUpdateOptions updateOptions = ObjectUpdateOptions.LateUpdate;
+        public ObjectDeltaTimeOptions deltaTimeOptions = ObjectDeltaTimeOptions.Normal;
 
+        [Range( 0f, 100f )] [Tooltip( "The lerp speed of the follower" )]
+        public float movingSpeed = 2f;
 
-        [Range(0f, 100f), Tooltip("The lerp speed of the follower")]
-        public float MovingSpeed = 2f;
-
-        [Range(0f, 100f), Tooltip("The rotation speed of the follower")]
-        public float RotationSpeed = 2f;
+        [Range( 0f, 100f )] [Tooltip( "The rotation speed of the follower" )]
+        public float rotationSpeed = 2f;
 
         public Vector3 offSet = Vector3.zero;
+
         #endregion
 
         #region Main Thread
 
         private void Update()
         {
-            if ( objectUpdateOptions == ObjectUpdateOptions.Update )
+            if ( updateOptions == ObjectUpdateOptions.Update )
                 FollowObject();
         }
 
         private void FixedUpdate()
         {
-            if ( objectUpdateOptions == ObjectUpdateOptions.FixedUpdate )
+            if ( updateOptions == ObjectUpdateOptions.FixedUpdate )
                 FollowObject();
         }
 
         private void LateUpdate()
         {
-            if ( objectUpdateOptions == ObjectUpdateOptions.LateUpdate )
+            if ( updateOptions == ObjectUpdateOptions.LateUpdate )
                 FollowObject();
         }
 
@@ -62,45 +78,59 @@ namespace CXUtils.HelperComponents
 
         #region Main Methods
 
-        public void FollowObject()
+        private void FollowObject()
         {
             FollowPos();
             FollowRotation();
         }
 
-        public void FollowPos()
+        private void FollowPos()
         {
-            Vector3 newPos = transformTo.position;
+            var newPos = transformTo.position;
 
             //check the has off set (if has then add)
-            if ( objectFollowPositionOptions == ObjectFollowPositionOptions.All || objectFollowPositionOptions == ObjectFollowPositionOptions.HasOffsetOnly )
+            if ( positionOptions == ObjectFollowPositionOptions.All || positionOptions == ObjectFollowPositionOptions.HasOffsetOnly )
                 newPos += offSet;
-            if ( objectFollowPositionOptions == ObjectFollowPositionOptions.All || objectFollowPositionOptions == ObjectFollowPositionOptions.HasLerpOnly )
-                newPos = Vector3.Lerp(transform.position, newPos, MovingSpeed / 100f);
+            
+            if ( positionOptions == ObjectFollowPositionOptions.All || positionOptions == ObjectFollowPositionOptions.HasLerpOnly )
+                newPos = Vector3.Lerp( transform.position, newPos, GetDeltaTime() * movingSpeed );
 
             //then just set it
             transform.position = newPos;
         }
 
-        public void FollowRotation()
+        private void FollowRotation()
         {
             //if none then just don't do anything
-            if ( objectFollowRotationOptions == ObjectFollowRotationOptions.None )
+            if ( rotationOptions == ObjectFollowRotationOptions.None )
                 return;
 
             //stores the target rotation
-            Quaternion newRot = transformTo.rotation;
+            var newRot = transformTo.rotation;
 
             //if (objectFollowRotationOptions == ObjectFollowRotationOptions.NoLerp) then do nothing
 
-            if ( objectFollowRotationOptions == ObjectFollowRotationOptions.HasLerp )
-                newRot = Quaternion.Lerp(transform.rotation, newRot, RotationSpeed / 100f);
+            if ( rotationOptions == ObjectFollowRotationOptions.HasLerp )
+                newRot = Quaternion.Lerp( transform.rotation, newRot, GetDeltaTime() * rotationSpeed );
 
             if ( newRot != default )
                 transform.rotation = newRot;
         }
 
+        private float GetDeltaTime()
+        {
+            switch (deltaTimeOptions)
+            {
+                case ObjectDeltaTimeOptions.None:     return 1f;
+                case ObjectDeltaTimeOptions.Normal:   return Time.deltaTime;
+                case ObjectDeltaTimeOptions.Fixed:    return Time.fixedDeltaTime;
+                
+                default:
+                    ExceptionUtils.GetException( ErrorType.NotAccessible );
+                    return float.NaN;
+            }
+        }
+        
         #endregion
-
     }
 }
