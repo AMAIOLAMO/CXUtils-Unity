64bb0856d44795c7ed85bb986995235c3c57af79
CXRedix <1700909931@qq.com>
2021-06-30 23:47:53 +0800

    added sequence for story n stuff

---
+++ Scripts/NonEditorScripts/HelperUtils/Sequence/CXSequence.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace CXUtils.CodeUtils
+{
+    public class CXSequence
+    {
+        readonly List<Action> _sequenceTriggerList;
+
+        int _sequenceIndex = 0;
+
+        public CXSequence()
+        {
+            _sequenceTriggerList = new List<Action>();
+        }
+
+        public CXSequence Append( Action trigger )
+        {
+            _sequenceTriggerList.Add( trigger );
+            return this;
+        }
+
+        /// <summary>
+        /// triggers to the next trigger, will return true if sequence ended
+        /// </summary>
+        public bool NextTrigger()
+        {
+            if ( _sequenceIndex >= _sequenceTriggerList.Count )
+                return true;
+            
+            _sequenceTriggerList[_sequenceIndex]?.Invoke();
+            return false;
+        }
+
+        public void Reset()
+        {
+            _sequenceIndex = 0;
+        }
+    }
+}
