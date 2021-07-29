f752a2a95a7c90e50c2aa584643db1f88c5f9e7f
CXRedix <1700909931@qq.com>
2021-07-29 14:42:54 +0800

    Added handles utils

---
+++ Scripts/Src/Utilities/Unity/Debug/HandleUtils.cs
@@ -0,0 +1,41 @@
+using CXUtils.Types;
+using CXUtils.Types.UnityUtils;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+#endif
+
+namespace CXUtils.CodeUtils
+{
+    /// <summary>
+    ///     An utility to draw stuff in <see cref="SceneView"/>
+    /// </summary>
+    public static class HandleUtils
+    {
+        public static void DrawTextRaw( string text, Float3 worldPosition, Color color, SceneView sceneView, GUIStyle style )
+        {
+#if UNITY_EDITOR
+            var screenPos = sceneView.camera.WorldToScreenPoint( worldPosition.ToUnity() );
+            var size = style.CalcSize( new GUIContent( text ) );
+            GUI.color = color;
+            GUI.Label( new Rect( screenPos.x - size.x * .5f, -screenPos.y + sceneView.position.height - size.y * 2f, size.x, size.y ), text, style );
+#endif
+        }
+
+        public static void DrawText( string text, Float3 worldPosition, Color color, GUIStyle style )
+        {
+#if UNITY_EDITOR
+            if ( SceneView.currentDrawingSceneView == null ) return;
+
+            Handles.BeginGUI();
+
+            DrawTextRaw( text, worldPosition, color, SceneView.currentDrawingSceneView, style );
+
+            Handles.EndGUI();
+#endif
+        }
+
+        public static void DrawText( string text, Float3 worldPosition ) => DrawText( text, worldPosition, Color.white, GUI.skin.box );
+    }
+}
