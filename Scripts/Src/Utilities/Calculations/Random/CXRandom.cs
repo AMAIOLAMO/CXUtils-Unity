a7bf637eafbc44db25f30e4d45eb050f402aa26e
CXRedix <1700909931@qq.com>
2021-08-07 16:32:55 +0800

    Updated random and changed names

--- Scripts/Src/Utilities/Calculations/Random/CXRandom.cs
+++ Scripts/Src/Utilities/Calculations/Random/CXRandom.cs
@@ -1,39 +1,39 @@
-﻿using System.Collections.Generic;
-using SysRandom = System.Random;
+﻿using System;
+using System.Collections.Generic;
 
 namespace CXUtils.Common
 {
     /// <summary>
-    ///     A <see cref="SysRandom" /> Wrapper
+    ///     A <see cref="Random" /> Wrapper
     /// </summary>
-    public class CxRandom : SysRandom
+    public class CxRandom : Random
     {
         public CxRandom() { }
-        public CxRandom( int seed ) : base( seed ) { }
+        public CxRandom(int seed) : base(seed) { }
 
         /// <summary>
         ///     Randomly decides and returns a boolean <br />
         ///     QUICK NOTE: excludes threshold (value > threshold)
         /// </summary>
-        public bool FlipCoin( double threshold = .5f ) => NextDouble() > threshold;
+        public bool FlipCoin(double threshold = .5f) => NextDouble() > threshold;
 
         /// <summary>
         ///     Randomly decides between two items <br />
         ///     QUICK NOTE: excludes threshold (value > threshold)
         /// </summary>
-        public T FlipCoin<T>( T t1, T t2, double threshold = .5d ) => FlipCoin( threshold ) ? t1 : t2;
+        public T FlipCoin<T>(T t1, T t2, double threshold = .5d) => FlipCoin(threshold) ? t1 : t2;
 
         /// <summary>
         ///     Randomly decides between these items <br />
         ///     QUICK NOTE: possibilities are the same
         /// </summary>
-        public T FlipCoin<T>( params T[] items ) => items[Next( 0, items.Length )];
+        public T Choose<T>(params T[] items) => items[Next(0, items.Length)];
 
         /// <summary>
         ///     Randomly decides between items with the given sorted pair <br />
         ///     QUICK NOTE: <paramref name="probabilityItemPair" /> needs to be sorted from lowest to highest
         /// </summary>
-        public bool TryFlipCoin<T>( out T item, params KeyValuePair<int, T>[] probabilityItemPair )
+        public bool TryChoose<T>(out T item, params KeyValuePair<int, T>[] probabilityItemPair)
         {
             int i, total = 0;
 
@@ -42,7 +42,7 @@ namespace CXUtils.Common
                 total += probabilityItemPair[i].Key;
 
             const int lastMin = 0;
-            int rand = Next( 0, total + 1 ); // this will go from 0 to tot (since next doesn't include upper bound, so we increment it)
+            int rand = Next(0, total + 1); // this will go from 0 to tot (since next doesn't include upper bound, so we increment it)
 
             //get probability
             for ( i = 0; i < probabilityItemPair.Length; i++ )
