df8fd67740dfea1b79af48205508df4f37731f15
CXRedix <1700909931@qq.com>
2021-08-19 16:57:29 +0800

    made random into an extension instead

--- Scripts/Src/Utilities/Calculations/Random/CXRandom.cs
+++ Scripts/Src/Utilities/Calculations/Random/CXRandom.cs
@@ -4,56 +4,61 @@ using System.Collections.Generic;
 namespace CXUtils.Common
 {
     /// <summary>
-    ///     A <see cref="Random" /> Wrapper
+    ///     An extension for the system random
     /// </summary>
-    public class CxRandom : Random
+    public static class CxRandomExtension
     {
-        public CxRandom() { }
-        public CxRandom(int seed) : base(seed) { }
-
-        public float NextFloat()
+        /// <summary>
+        ///     Gives a random number between <see cref="float.MinValue"/> ~ <see cref="float.MaxValue">
+        /// </summary>
+        public static float NextFloat(this Random random)
         {
-            double mantissa = NextDouble() * 2d - 1d;
+            double mantissa = random.NextDouble() * 2d - 1d;
 
-            double exponent = Math.Pow(2d, Next(-126, 128));
+            double exponent = Math.Pow(2d, random.Next(-126, 128));
             return (float)(mantissa * exponent);
         }
+
+        /// <summary>
+        ///     Gives a random number between <paramref name="min"/> ~ <paramref name="max"/>
+        /// </summary>
+        public static float NextFloat(this Random random, float min, float max) => (float)random.NextDouble() * (max - min) + min;
 
         /// <summary>
         ///     Randomly decides and returns a boolean <br />
         ///     QUICK NOTE: excludes threshold (value > threshold)
         /// </summary>
-        public bool FlipCoin(double threshold = .5f) => NextDouble() > threshold;
+        public static bool FlipCoin(this Random random, double threshold = .5f) => random.NextDouble() > threshold;
 
         /// <summary>
         ///     Randomly decides between two items <br />
         ///     QUICK NOTE: excludes threshold (value > threshold)
         /// </summary>
-        public T FlipCoin<T>(T t1, T t2, double threshold = .5d) => FlipCoin(threshold) ? t1 : t2;
+        public static T FlipCoin<T>(this Random random, T t1, T t2, double threshold = .5d) => random.FlipCoin(threshold) ? t1 : t2;
 
         /// <summary>
         ///     Randomly decides between these items <br />
         ///     QUICK NOTE: possibilities are the same
         /// </summary>
-        public T Choose<T>(params T[] items) => items[Next(0, items.Length)];
+        public static T Choose<T>(this Random random, params T[] items) => items[random.Next(0, items.Length)];
 
         /// <summary>
         ///     Randomly decides between items with the given sorted pair <br />
         ///     QUICK NOTE: <paramref name="probabilityItemPair" /> needs to be sorted from lowest to highest
         /// </summary>
-        public bool TryChoose<T>(out T item, params KeyValuePair<int, T>[] probabilityItemPair)
+        public static bool TryChoose<T>(this Random random, out T item, params KeyValuePair<int, T>[] probabilityItemPair)
         {
             int i, total = 0;
 
             //get total
-            for ( i = 0; i < probabilityItemPair.Length; i++ )
+            for ( i = 0; i < probabilityItemPair.Length; ++i )
                 total += probabilityItemPair[i].Key;
 
             const int lastMin = 0;
-            int rand = Next(0, total + 1); // this will go from 0 to tot (since next doesn't include upper bound, so we increment it)
+            int rand = random.Next(0, total + 1); // this will go from 0 to tot (since next doesn't include upper bound, so we increment it)
 
             //get probability
-            for ( i = 0; i < probabilityItemPair.Length; i++ )
+            for ( i = 0; i < probabilityItemPair.Length; ++i )
                 //if in range
                 if ( rand > lastMin && rand <= total )
                 {
