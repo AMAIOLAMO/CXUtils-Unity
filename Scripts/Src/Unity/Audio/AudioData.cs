36c1a0e93ded704dd366bb852407954be11c598a
CXRedix <1700909931@qq.com>
2021-08-21 12:08:06 +0800

    Revamped Icons

--- Scripts/Src/Unity/Audio/AudioData.cs
+++ Scripts/Src/Unity/Audio/AudioData.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 namespace CXUtils.Components
@@ -8,25 +9,23 @@ namespace CXUtils.Components
     [CreateAssetMenu( fileName = "audioData", menuName = "CXUtils/Audio/AudioData" )]
     public class AudioData : ScriptableObject
     {
-        [Header( "Audio Clips" )]
-        [SerializeField] AudioClip[] audioClips;
+        [FormerlySerializedAs("audioClips")] [SerializeField] AudioClip[] _audioClips;
 
-        public AudioClip[] AudioClips => audioClips;
+        public AudioClip[] AudioClips => _audioClips;
 
         /// <summary>
         ///     Does this audio data have no audio clips
         /// </summary>
-        public bool HasAudioClip => audioClips.Length > 0;
+        public bool HasAudioClip => _audioClips.Length > 0;
 
         /// <summary>
         ///     Gets a audio clip from the audio clips
         /// </summary>
-        public AudioClip GetAudioClip( int index = 0 ) =>
-            audioClips[index];
+        public AudioClip GetAudioClip( int index = 0 ) => _audioClips[index];
 
         /// <summary>
         ///     Get's a random audio clip from the audio clips
         /// </summary>
-        public AudioClip GetRandomAudioClip() => !HasAudioClip ? null : audioClips[Random.Range( 0, audioClips.Length )];
+        public AudioClip GetRandomAudioClip() => !HasAudioClip ? null : _audioClips[Random.Range( 0, _audioClips.Length )];
     }
 }
